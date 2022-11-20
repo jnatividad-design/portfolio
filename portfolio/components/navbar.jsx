e6c69b5e8cdb4b2a09e17635a8935e4f59aec420
jnatividad-design <joshuamnatividad@yahoo.com>
2022-11-19 19:38:41 -0500

    Fix hamburger menu

--- portfolio/components/navbar.jsx
+++ portfolio/components/navbar.jsx
@@ -24,26 +24,23 @@ const handleClick = () => {
               </span>
             </a>
           </Link>
-          <button onclick= {handleClick}>Test </button>
-          <button className=' inline-flex p-3 hover:bg-blue-500 rounded lg:hidden text-white ml-auto hover:text-white outline-none' onClick={handleClick}>
+<button className='inline-flex p-3 hover:bg-blue-500 rounded lg:hidden text-white ml-auto hover:text-white outline-none' onClick={()=>handleClick()}>
             <svg
               className='w-6 h-6'
               fill='none'
               stroke='currentColor'
               viewBox='0 0 24 24'
               xmlns='http://www.w3.org/2000/svg'
-              onClick={handleClick}
             >
               <path
                 strokeLinecap='round'
                 strokeLinejoin='round'
                 strokeWidth={2}
                 d='M4 6h16M4 12h16M4 18h16'
-                onClick={handleClick}
               />
             </svg>
           </button>
-          <div className={`${
+          <div className={` ${
             navbar ? '' : 'hidden'
           }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}>
             <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
@@ -74,7 +71,6 @@ const handleClick = () => {
               </Link>
             </div>
           </div>
-          <button>TEST</button>
         </nav>
       </>
     );
