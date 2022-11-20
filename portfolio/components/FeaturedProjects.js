49f77e200ef6adbdf290525c903a9d0ff12a92fd
jnatividad-design <joshuamnatividad@yahoo.com>
2022-11-20 15:05:04 -0500

    Add image component

--- portfolio/components/FeaturedProjects.js
+++ portfolio/components/FeaturedProjects.js
@@ -1,14 +1,90 @@
 import React from 'react';
 import Link from 'next/Link';
+import Image from 'next/image'
 
 export default function FeaturedProjects() {
     return (
         <div className="bg-[#F1F1F1] -mt-40 dark:bg-gray-900">
         <div className="max-w-6xl mx-auto">
-          <header className="flex flex-col md:flex-row justify-between items-center pt-40 mx-10 md:my-20 lg:my-0">
+          <header className="flex flex-col md:flex-row justify-between items-left pt-40 mx-10 md:my-20 lg:my-0">
             <h1 className="text-5xl lg:text-9xl max-w-lg font-bold text-gray-500 my-20 md:my-0 md:text-white dark:text-gray-600 text-left">
               Featured Projects
             </h1>
+          </header>
+  
+          {/* Grid starts here  */}
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-8 lg:-mt-8 pb-40">
+            {/* Single card */}
+            <a
+              href="/"
+              className="w-full block col-span-3 shadow-2xl"
+            >
+              <div className="relative overflow-hidden">
+                <Image
+                src='/virtuoso.jpg'
+                alt='Virtuoso'
+                height={344}
+                width={580}
+                />
+                <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
+                  Virtuoso
+                </h1>
+                <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
+                  01
+                </h1>
+              </div>
+            </a>
+            {/* Single card */}
+            <a
+              href="https://placeholdertech.in"
+              className="w-full block col-span-3  sm:col-span-2 shadow-2xl"
+            >
+              <div className="relative overflow-hidden">
+                {/* <div className="overlay absolute inset-0 bg-black bg-opacity-70 z-10"></div> */}
+                <Image
+                src='/grad.jpg'
+                alt='Prewrecks'
+                height={344}
+                width={580}
+                />
+                
+                <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
+                  PreWrecks
+                </h1>
+                <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
+                  02
+                </h1>
+              </div>
+            </a>
+            {/* Single card */}
+            <a
+              href="https://manuarora.in"
+              className="w-full block col-span-3 sm:col-span-1  object-cover"
+            >
+              <div className="relative overflow-hidden shadow-2xl">
+                {/* <div className="overlay absolute inset-0 bg-black bg-opacity-70 z-10"></div> */}
+                <Image
+                src='/2048.jpg'
+                alt='Virtuoso'
+                height={344}
+                width={580}
+                />
+                {/* img code in case I need to revert back
+                <img
+                src="../public/2048.jpg"
+                alt="portfolio"
+                className="transform hover:scale-125 transition duration-2000 ease-out object-cover shadow-2xl"
+                />
+            */}
+                <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
+                  2048
+                </h1>
+                <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
+                  03
+                </h1>
+              </div>
+            </a>
+          </div>
             <Link href="/projects">
               <a className="mb-20 md:mb-0 px-8 py-4 rounded-md bg-white shadow-lg text-xl font-semibold flex flex-row space-x-4 items-center dark:text-gray-700">
                 <svg
@@ -29,70 +105,6 @@ export default function FeaturedProjects() {
                 <p>View all</p>
               </a>
             </Link>
-          </header>
-  
-          {/* Grid starts here */}
-          <div className="grid md:grid-cols-3 gap-8 lg:-mt-8 pb-40">
-            {/* Single card */}
-            <a
-              href="https://tailwindmasterkit.com"
-              className="w-full block col-span-3 shadow-2xl"
-            >
-              <div className="relative overflow-hidden">
-                <img
-                  src="/tmk.jpg"
-                  alt="portfolio"
-                  className="transform hover:scale-125 transition duration-2000 ease-out"
-                />
-                <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
-                  Virtuoso
-                </h1>
-                <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
-                  01
-                </h1>
-              </div>
-            </a>
-            {/* Single card */}
-            <a
-              href="https://placeholdertech.in"
-              className="w-full block col-span-3  sm:col-span-2 shadow-2xl"
-            >
-              <div className="relative overflow-hidden">
-                {/* <div className="overlay absolute inset-0 bg-black bg-opacity-70 z-10"></div> */}
-                <img
-                  src="/placeholdertech.png"
-                  alt="portfolio"
-                  className="transform hover:scale-125 transition duration-2000 ease-out"
-                />
-                <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
-                  PreWrecks
-                </h1>
-                <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
-                  02
-                </h1>
-              </div>
-            </a>
-            {/* Single card */}
-            <a
-              href="https://manuarora.in"
-              className="w-full block col-span-3 sm:col-span-1  object-cover"
-            >
-              <div className="relative overflow-hidden shadow-2xl">
-                {/* <div className="overlay absolute inset-0 bg-black bg-opacity-70 z-10"></div> */}
-                <img
-                  src="/portfolio.png"
-                  alt="portfolio"
-                  className="transform hover:scale-125 transition duration-2000 ease-out object-cover shadow-2xl"
-                />
-                <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
-                  2048
-                </h1>
-                <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
-                  03
-                </h1>
-              </div>
-            </a>
-          </div>
         </div>
       </div>
     );
